617d014891009d14ace0e8af555f2385297a6d36
Mack Gregory <mgr@ucdavis.edu>
2022-08-25 20:42:21 -0500

    Added ESLint using the standard styleguide

--- database.js
+++ database.js
@@ -1,80 +1,83 @@
 'use strict'
 
-const sql = require('sqlite3');
-const util = require('util');
+const sql = require('sqlite3')
+const util = require('util')
 
 // This can be replaced with a filename for a persistent database.
-const db = new sql.Database(":memory:");
+const db = new sql.Database(':memory:')
 
 // Check if database exists
-const cmd = " SELECT name FROM sqlite_master WHERE type='table' AND name='ActivityTable' ";
+const cmd = " SELECT name FROM sqlite_master WHERE type='table' AND name='ActivityTable' "
 
-db.get(cmd, function(err, val) {
-  if (val == undefined) {
-    console.log("No profile table - creating one");
-    createActivityTable();
-  } else {
-    console.log("Activity table found");
+db.get(cmd, function (err, val) {
+  if (err) {
+    console.log('Problem accessing the SQL server:', err)
   }
-});
+  if (val === undefined) {
+    console.log('No profile table - creating one')
+    createActivityTable()
+  } else {
+    console.log('Activity table found')
+  }
+})
 
-function createActivityTable() {
-  const cmd = 'CREATE TABLE ActivityTable (rowIdNum INTEGER PRIMARY KEY, activity TEXT, date INTEGER, amount FLOAT)';
+function createActivityTable () {
+  const cmd = 'CREATE TABLE ActivityTable (rowIdNum INTEGER PRIMARY KEY, activity TEXT, date INTEGER, amount FLOAT)'
   db.run(cmd, (err, val) => {
     if (err) {
-      console.log("Acitivity database creation error", err.message);
+      console.log('Acitivity database creation error', err.message)
     } else {
-      console.log("Created Activity database");
+      console.log('Created Activity database')
     }
-  });
+  })
 }
 
-db.run = util.promisify(db.run);
-db.get = util.promisify(db.get);
-db.all = util.promisify(db.all);
+db.run = util.promisify(db.run)
+db.get = util.promisify(db.get)
+db.all = util.promisify(db.all)
 
-const insertDB = "insert into ActivityTable (activity, date, amount) values (?,?,?)";
-const getDB = "select * from ActivityTable where activity = ?";
-const deleteOneDB = "delete from ActivityTable where rowIdNum = ?";
+const insertDB = 'insert into ActivityTable (activity, date, amount) values (?,?,?)'
+const getDB = 'select * from ActivityTable where activity = ?'
+const deleteOneDB = 'delete from ActivityTable where rowIdNum = ?'
 
-async function postActivity(activity, date, amount) {
+async function postActivity (activity, date, amount) {
   try {
-    await db.run(insertDB, [activity, date, amount]);
+    await db.run(insertDB, [activity, date, amount])
   } catch (error) {
-    console.log("Error inserting to DB:", error);
+    console.log('Error inserting to DB:', error)
   }
 }
 
-async function getActivity(activity) {
+async function getActivity (activity) {
   try {
-    let results = await db.get(getDB, activity);
-    return (results.rowIdNum != null) ? results : null;
+    const results = await db.get(getDB, activity)
+    return (results.rowIdNum != null) ? results : null
   } catch (error) {
-    console.log("Error getting one from database:", error);
-    return null;
+    console.log('Error getting one from database:', error)
+    return null
   }
 }
 
-async function getAllActivities(activity) {
+async function getAllActivities (activity) {
   try {
-    let results = await db.all(getDB, activity);
-    return (results[0] != null) ? results : null;
+    const results = await db.all(getDB, activity)
+    return (results[0] != null) ? results : null
   } catch (error) {
-    console.log("Error getting all", activity, "activities from the database:", error);
-    return null;
+    console.log('Error getting all', activity, 'activities from the database:', error)
+    return null
   }
 }
 
-async function deleteActivity(rowIdNum) {
+async function deleteActivity (rowIdNum) {
   try {
-    await db.run(deleteOneDB, [rowIdNum]);
+    await db.run(deleteOneDB, [rowIdNum])
   } catch (error) {
-    console.log("Error deleting activity with rowIdNum =", rowIdNum, ":", error);
+    console.log('Error deleting activity with rowIdNum =', rowIdNum, ':', error)
   }
 }
 
-db.postActivity = postActivity;
-db.getActivity = getActivity;
-db.getAllActivities = getAllActivities;
-db.deleteActivity = deleteActivity;
-module.exports = db;
+db.postActivity = postActivity
+db.getActivity = getActivity
+db.getAllActivities = getAllActivities
+db.deleteActivity = deleteActivity
+module.exports = db
